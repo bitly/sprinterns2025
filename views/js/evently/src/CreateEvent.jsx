e2f4c23cf89f48986fc90bd27ab5377a0430dae2
Tanzina Sumona <71103616+TanzinaS@users.noreply.github.com>
2025-01-10 15:27:16 -0500

    In the create event.CSS I added the appropriate CSS to align the event type input with the rest of the form. For JSX I added a field where you can input the type of event. For the event_table.go I corrected the variables for the event type so it can be called

--- views/js/evently/src/CreateEvent.jsx
+++ views/js/evently/src/CreateEvent.jsx
@@ -10,6 +10,7 @@ function CreateEvent() {
   const [DescriptionForm, setDescriptionForm] = useState("");
   const [AttendeesForm, setAttendeesForm] = useState("");
   const [PublicPrivate, setPublicPrivate] = useState("");
+  const [EventType, setEventType] = useState(""); // TS
   const [HostName, setHostName] = useState("");
   const [ContactForm, setContactForm] = useState("");
   const [imageUrl, setImageUrl] = useState("");
@@ -24,7 +25,7 @@ function CreateEvent() {
         method: "POST",
         mode: "cors",
         body: JSON.stringify({
-          title: EventTitle, date: DateForm, time: TimeForm, location: LocationForm, host_name: HostName, description: DescriptionForm, contact_info: ContactForm, public_private: "public", max_attendees: parseInt(AttendeesForm), image_url: imageUrl,
+          title: EventTitle, date: DateForm, time: TimeForm, location: LocationForm, host_name: HostName, description: DescriptionForm, contact_info: ContactForm, public_private: "public", typeOfevent:EventType, max_attendees: parseInt(AttendeesForm), image_url: imageUrl,
         }),
       });
       if (res.status === 201) {
@@ -52,7 +53,7 @@ function CreateEvent() {
             <div className='event-title'>
              <h4>Event title</h4>
            </div>
-          
+
               <input value={EventTitle} className = "create-input-boxes" onChange={(e) => {
                 setEventTitle(e.target.value)}}/> 
 
@@ -85,6 +86,7 @@ function CreateEvent() {
                 <div className="public-status">
                   <h4>Public/Private</h4>
                   <select className = "create-input-boxes" value={PublicPrivate} onChange={(e) => setPublicPrivate(e.target.value)}>
+                    <option name="Blank" value="Blank" >    </option>
                     <option name="public" value="public" > public</option>
                     <option name="private" value="private" >private</option>
                   </select>
@@ -95,8 +97,19 @@ function CreateEvent() {
               <h4>Host name</h4>
               <input value={HostName} className = "create-input-boxes" onChange={(e) => setHostName(e.target.value)}/>
                 </div>
+
+                <div className='event-type-selection'> 
+              <h4>What type of event is this?</h4>
+              <select className = "create-input-boxes" value={EventType} onChange={(e) => setEventType(e.target.value)}>
+                    <option name="Blank" value="Blank" >    </option>
+                    <option name="Social" value="social" > Social</option>
+                    <option name="Business" value="business" >Business</option>
+                    <option name="Education" value="education" >Education</option>
+                    <option name="Arts & Recreation" value="arts & recreation" >Arts & Recreation</option>
+                  </select>
+                </div>
               
-              <div className='contact-info'>
+              <div className='contact-info'> 
               <h4>Contact info</h4>
               <input value={ContactForm} className = "create-input-boxes" onChange={(e) => setContactForm(e.target.value)}/>
               </div>
