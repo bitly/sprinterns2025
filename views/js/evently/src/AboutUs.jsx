71be4e836f4e93df991bdb6fe17db1bec299331e
Juana Casey <juana.casey@jjay.cuny.edu>
2025-01-14 12:05:20 -0500

    Updated AboutUs section.

--- views/js/evently/src/AboutUs.jsx
+++ views/js/evently/src/AboutUs.jsx
@@ -19,11 +19,11 @@ function AboutUs() {
             {/* Profile Sections */}
             <div className="profilesSection">
                 <div className="profileContainer">
-                    <div className="profileImages">
+                    <div className="profileFront">
                         <img src={profileImage1} alt="Juana Casey" />
+                          <h2>Juana Casey</h2>
                     </div>
                     <div className="personInfo">
-                        <h2>Juana Casey</h2>
                         <p>Hello, my name is Juana Casey! I am a second-year student at CUNY John Jay College of Criminal Justice, where I am majoring in Computer Science with a focus on Information Security. When I am not in school, you can usually find me at my computer working on video editing projects. I also enjoy creating floral arrangements and reading in my free time. I aspire to build a career in Front-End Development and Digital Forensics. </p>
                         <p>Feel free to connect with me on LinkedIn!</p>
                         <a href="https://www.linkedin.com/in/juana-casey/">LinkedIn</a>
@@ -31,11 +31,11 @@ function AboutUs() {
                 </div>
 
                 <div className="profileContainer">
-                    <div className="profileImages">
+                    <div className="profileFront">
                         <img src={profileImageSara} alt="Sara Abdulla" />
+                        <h2>Sara Abdulla</h2>
                     </div>
                     <div className="personInfo">
-                        <h2>Sara Abdulla</h2>
                         <p>I'm a sophomore at CUNY Lehman College and Macaulay Honors College studying Computer Graphics and Imaging with a minor in Media Communications Systems. I aspire to work in a space where I can leverage my creativity to craft designs and campaigns that deliver impactful experiences, connecting with and inspiring diverse audiences.</p>
                         <p>Let's Connect on LinkedIn!</p>
                         <a href="https://www.linkedin.com/in/sara-abdulla-0ba97b292">LinkedIn</a>
@@ -43,11 +43,11 @@ function AboutUs() {
                 </div>
 
                 <div className="profileContainer">
-                    <div className="profileImages">
+                    <div className="profileFront">
                         <img src={profileImageJennifer} alt="Jennifer Perez-Juarez" />
+                        <h2>Jennifer Perez-Juarez</h2>
                     </div>
                     <div className="personInfo">
-                        <h2>Jennifer Perez-Juarez</h2>
                         <p>Hi, my name is Jennifer Perez-Juarez and I am currently a sopphmore studying computer science at CUNY Brooklyn College! One of my favorite things to do on campus is to find events on campus in search of free food and merchandise! Sometimes you'll even find me attending 2 - 3 events back-to-back on the same day! If you ever want to know the latest event to attend in hopes of gaining a free meal or merchandise to add to your collection, just reach out to me. </p>
                         <p>Connect with me on LinkedIn!</p>
                         <a href="http://www.linkedin.com/in/jennifer-perez-juarez" target="_blank" rel="noopener noreferrer">LinkedIn</a>
@@ -55,11 +55,11 @@ function AboutUs() {
                 </div>
 
                 <div className="profileContainer">
-                    <div className="profileImages">
+                    <div className="profileFront">
                         <img src={profileImageTS} alt="Tanzina Sumona" />
+                        <h2>Tanzina Sumona</h2>
                     </div>
                     <div className="personInfo">
-                        <h2>Tanzina Sumona</h2>
                         <p>My name is Tanzina, and I am a sophomore at CCNY majoring in computer science. I am interested in learning about data science and analysis, or just working with large datasets and uncovering trends through predictive modeling</p>
                         <p>Connect with me on LinkedIn!</p>
                         <a href="https://www.linkedin.com/in/tanzina-sumona/">LinkedIn</a>
@@ -67,11 +67,11 @@ function AboutUs() {
                 </div>
 
                 <div className="profileContainer">
-                    <div className="profileImages">
+                    <div className="profileFront">
                         <img src={profileImageTran} alt="Tran Vo" />
+                        <h2>Tran Vo</h2>
                     </div>
                     <div className="personInfo">
-                        <h2>Tran Vo</h2>
                         <p> Hi, I’m Tran Vo! I’m currently studying Computer Science at BMCC,
                             and when I’m not buried in code or classwork, you’ll probably find
                             me hanging out with friends, exploring new restaurants, or getting
