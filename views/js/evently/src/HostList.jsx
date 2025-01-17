a2957ae35b4fd0571632617f5cf640edc217fafe
Jennifer Perez-Juarez <jenniferperez.6190@gmail.com>
2025-01-17 16:57:17 -0500

    Added some CSS to make people page look nicer

--- views/js/evently/src/HostList.jsx
+++ views/js/evently/src/HostList.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./HostList.css";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import profileImage1 from "./profile_img.jpg"
 
 function HostList() {
   const [attendeeData, setAttendeeData] = useState([]);
@@ -34,198 +33,42 @@ function HostList() {
   // };
 
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <>
-      {/* <div className="attendees-nav-container">
-        <h2 className="browse-title"> Browse Events</h2>
-        <div className="search-bar">
-          <input type="text" placeholder="Start Your Search Here!" />
-          <button className="search-btn"> 🔍 </button>
-        </div>
-
-        <div className="link-container">
-          <button className="btn"> All Events </button>
-          <button className="btn"> Social </button>
-          <button className="btn"> Business </button>
-          <button className="btn"> Education </button>
-          <button className="btn"> Arts & Recreation </button>
-        </div>
-      </div> */}
-
-      {error ? (
-        <div className="error-display">
-          <p> Error! Something went wrong loading attendeess. </p>
-          <p>Please try again!</p>
-          <Link to="/">
-            <p>Home</p>
-          </Link>
-        </div>
-      ) : (
-        <div className="public-attendees-container">
-          <div className="attendees-row">
-            {attendeeData.map((attendees) => (
-              <div
-                key={attendees.attendees_id} //Either attendees.attendees_id or attendee.id.
-                className="attendees-card"
-                // onClick={() => handleEventClick(attendees)}
-              >
-                <p className="attendees-card-name"> {attendees.first_name} {attendees.last_name}</p>
-                <div className="attendees-img">
-                  <img
-                    src={
-                      attendees.image_url !== "" ? attendees.image_url : "default.png"
-                    }
-                    alt="User inputed description."
-                  />
-                </div>
-                <div className="email-and-phone-number-container">
-                  <h4> Phone Number: {attendees.phone_number}</h4>
-                  <h4> Email: {attendees.email}</h4>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-
-      {/* {selectEvent && <RSVPButton attendees={selectEvent} />} */}
-    </>
-=======
     <div>
-=======
-    <div className="all-hosts-container">
->>>>>>> ae54a67 (Trying to figure out how to revamp this page with ChatGPT, needs a lot of work)
       <div className="header">
         <h1>Discover New People</h1>
-      </div>
-
-     {/* <table>
-        <thead>
+        <Link to="/create-host" className="host-button">Join now</Link>
+      </div>  
+  
+      <table>
+        <thead className="column-names">
           <tr>
             <th>Profile</th>
             <th>First Name</th>
             <th>Last Name</th>
-            <th>Email</th>
+            <th>Email</th> 
           </tr>
         </thead>
         <tbody>
-          {hosts.length > 0 ? (
-            hosts.map((host) => (
-              <tr key={host.host_id}>
-                <td className="profile-img">
+          {hosts.map(host => (
+            <tr key={host.host_id}>
+              <td>
+                <div className="profile-img">
                   <img
                     src={
                       host.image_url !== "" ? host.image_url : "default.png"
                     }
                     alt={`Image for ${host.first_name} ${host.last_name}`}
                   />
-                </td>
-                <td>{host.first_name}</td>
-                <td>{host.last_name}</td>
-                <td>{host.email}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="4" style={{ textAlign: "center" }}>
-                No hosts available
+                </div>
               </td>
+              <td>{host.first_name}</td>
+              <td>{host.last_name}</td>
+              <td>{host.email}</td>    
             </tr>
-          )}
+          ))}
         </tbody>
-      </table>
-*/}
-
-
-
-      <div class="nav-bar-container">
-      <Link to="/create-host" className="host-button">Join now</Link>
-        <div class="account-search-bar">
-        <span class="account-search-btn"> 🔍</span>
-          <input class="account-search-input" type="text" placeholder="Start Your Search Here!" />
-        </div>
-      </div>
-
-
-        <div class="peopleSection">
-
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>Attendee</p>
-            <h2>John Doe</h2>
-            <h2>fake@gmail.com</h2>
-        </div>
-        
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-            
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-        <div class="accountContainer">
-          <div class="accountInfo">
-            <img src={profileImage1} alt="Profile picture" className="profile-image" />
-          </div>
-            <p>John Doe</p>
-        </div>
-
-
-
-      </div>
-
-
-
-
+      </table>  
     </div>
->>>>>>> d3c9750 (Worked on frontend for people page + form page to play around with design implmenetation)
   );
   
 }
