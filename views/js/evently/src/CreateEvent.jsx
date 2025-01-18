1a2d18347718b192281c3fecbdae1a059df1637c
Juana Casey <juana.casey@jjay.cuny.edu>
2025-01-17 21:43:08 -0500

    Updated the CreateEvent CSS & JSX files.

--- views/js/evently/src/CreateEvent.jsx
+++ views/js/evently/src/CreateEvent.jsx
@@ -5,13 +5,15 @@ import './CreateEvent.css';
 function CreateEvent() {
   const [EventTitle, setEventTitle ] = useState("Untitled Event");
   const [image, setImage] = useState(null);
+  const [isSelected, setIsSelected] = useState(false);
   const [DateForm, setDateForm] = useState("Set a Date...");
   const [TimeForm, setTimeForm] = useState("TIME");
   const [LocationForm, setLocationForm] = useState("LOCATION");
-  const [DescriptionForm, setDescriptionForm] = useState("DESCRIPTION");
-  const [AttendeesForm, setAttendeesForm] = useState("");
+  const [DescriptionForm, setDescriptionForm] = useState("Description");
+  const [AttendeesForm, setAttendeesForm] = useState("MAX ATTENDEES");
+  const [InvitesForm, setInvitesForm] = useState("");
   const [PublicPrivate, setPublicPrivate] = useState("");
-  const [EventType, setEventType] = useState(""); // TS
+  const [EventType, setEventType] = useState(""); //TS
   const [HostName, setHostName] = useState("HOST NAME");
   const [ContactForm, setContactForm] = useState("Contact");
   const [imageUrl, setImageUrl] = useState("Image URL");
@@ -26,7 +28,7 @@ function CreateEvent() {
         method: "POST",
         mode: "cors",
         body: JSON.stringify({
-          title: EventTitle, date: DateForm, time: TimeForm, location: LocationForm, host_name: HostName, description: DescriptionForm, contact_info: ContactForm, public_private: "public", event_type:EventType, max_attendees: parseInt(AttendeesForm), image_url: imageUrl,
+          title: EventTitle, date: DateForm, time: TimeForm, invites:InvitesForm, location: LocationForm, host_name: HostName, description: DescriptionForm, contact_info: ContactForm, public_private: "public", event_type:EventType, max_attendees: parseInt(AttendeesForm), image_url: imageUrl,
         }),
       });
       if (res.status === 201) {
@@ -43,9 +45,11 @@ function CreateEvent() {
       console.log(err);
     }
   }
-
+  
+  //Default Image
   const defaultImage = 'https://images.pexels.com/photos/16220888/pexels-photo-16220888/free-photo-of-birthday-party-catering.jpeg';
 
+  //Function that handles if user wants to change image
   const handleImageChange = (e) => 
   {
     const file = e.target.files[0];
@@ -60,6 +64,11 @@ function CreateEvent() {
     }
   };
 
+  const toggleState = () =>
+  {
+    setIsSelected(prevState => !prevState);
+  }
+
     return (
       <div className="create-event"> 
           <div className = "create-form-container">
@@ -69,23 +78,52 @@ function CreateEvent() {
               <input value={EventTitle} className = "create-input-boxes" onChange={(e) => {
                   setEventTitle(e.target.value)}}/> 
                   <div className="date">
-                    <input type="date" value={DateForm} className = "create-input-boxes" placeholder='(mm/dd/yyyy)' onChange={(e) => setDateForm(e.target.value)}/> 
+                    <input  
+                      type="date" 
+                      value={DateForm} 
+                      className = "create-input-boxes" 
+                      placeholder= "(mm/dd/yyyy)" 
+                      onChange={(e) => setDateForm(e.target.value)}/> 
                   </div>
                </div> 
             </div>
               
 
-              <div className="hostName-time-location-description-container">
+              <div className="name-attendees-time-location-description-container">
                 <div className='host-name'>
-                  <input value={HostName} className = "create-input-boxes" onChange={(e) => setHostName(e.target.value)}/>
+                  <input 
+                    value={HostName} 
+                    className = "create-input-boxes" 
+                    onChange={(e) => setHostName(e.target.value)}
+                    placeholder = "Host Name"
+                  />
+                </div>
+
+                <div className="max-attendees">
+                  <input 
+                    value={AttendeesForm} 
+                    className = "create-input-boxes" 
+                    onChange={(e) => setInvitesForm(e.target.value)}
+                  />
                 </div>
                 
                   <div className="time">
-                    <input type="time" value={TimeForm} className = "create-input-boxes time-input" onChange={(e) => setTimeForm(e.target.value)}/>
+                    <input 
+                      type="time" 
+                      value={TimeForm}
+                      className = "create-input-boxes time-input" 
+                      onChange={(e) => setTimeForm(e.target.value)}
+                      placeholder = "Time"
+                    />
                   </div>
 
                   <div className='location'>
-                    <input value={LocationForm} className = "create-input-boxes" onChange={(e) => setLocationForm(e.target.value)}/>
+                    <input 
+                      value={LocationForm} 
+                      className = "create-input-boxes" 
+                      onChange={(e) => setLocationForm(e.target.value)}
+                      placeholder= "Location"
+                    />
                   </div>
 
                   <div className='description'>
@@ -95,16 +133,34 @@ function CreateEvent() {
                       onChange={(e) => setDescriptionForm(e.target.value)}
                       rows = "5"
                       cols = "50"
+                      placeholder= "Description"
                     />
                 </div>
               </div>
 
 
-              <div className="max-attendees-private-public-container">
-                <div className="max-attendees">
-                  <h4>Max attendees</h4>
-                  <input value={AttendeesForm} className = "create-input-boxes" onChange={(e) => setAttendeesForm(e.target.value)}/>
+              <div className="invites-private-public-container">
+                {/* Invites that are either on and off */}
+                <div className="invites">
+                  <h4>Invites</h4>
+                    <div 
+                      className = "toggle-section"
+                      id = "create-input-boxes" 
+                      value={PublicPrivate} 
+                      onChange={(e) => setPublicPrivate(e.target.value)}
+                      
+                    >
+                        <button
+                          type = "button"
+                          className = "toggle-button"
+                          onClick={toggleState}
+                        >
+                          {isSelected ? "On" : "Off"}
+                        </button>
+                    </div>
                 </div>
+
+
                 <div className="public-status">
                   <h4>Public/Private</h4>
                   <select className = "create-input-boxes" value={PublicPrivate} onChange={(e) => setPublicPrivate(e.target.value)}>
@@ -155,3 +211,4 @@ function CreateEvent() {
   }
 
 export default CreateEvent;
+
